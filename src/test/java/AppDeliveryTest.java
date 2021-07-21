8736c689f149145941401bacfd6cafeb977399df
Rustem Mirzakaev <rustem.mirzakaev@gmail.com>
2021-07-22 02:50:59 +0500

    Task №2 added

--- src/test/java/AppDeliveryTest.java
+++ src/test/java/AppDeliveryTest.java
@@ -1,6 +1,7 @@
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.AfterEach;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -14,33 +15,68 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class AppDeliveryTest {
 
-    //  This is logic for the date
-    LocalDate date = LocalDate.now().plusDays(4);
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-    String formatted = formatter.format(date);
+    //    This is date logic
+    public String date(int days, String format) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(format));
+    }
 
-    //  This is variable for the delete values in input
-    String deleteString = Keys.chord(Keys.CONTROL, "a") + Keys.DELETE;
+    // This is logic for delete string value
+    public String delete() {
+        return Keys.chord(Keys.CONTROL, "a") + Keys.DELETE;
+    }
 
-
-    @AfterEach
+    @BeforeEach
     void setUp() {
         open("http://localhost:9999/");
 
     }
 
     @Test
+    @DisplayName("Meet reserved through 3 days")
     void shouldReservedDate() {
-        open("http://localhost:9999/");
+
+//      Test configuration
+        String meetDate = date(3, "dd.MM.yyyy");
+        String deleteString = delete();
+
+//      Test data
         $("[data-test-id='city'] .input__box .input__control[placeholder='Город']").setValue("Уфа");
-        $("[data-test-id='date'] .input__box .input__control[placeholder='Дата встречи']").setValue(deleteString);
-        $("[data-test-id='date'] .input__box .input__control[placeholder='Дата встречи']").setValue(String.valueOf(formatted));
+        $("[data-test-id='date'] .input__box .input__control[placeholder='Дата встречи']")
+                .setValue(deleteString);
+        $("[data-test-id='date'] .input__box .input__control[placeholder='Дата встречи']")
+                .setValue(String.valueOf(meetDate));
         $("[data-test-id='name'] .input__box .input__control[name='name']").setValue("Семенов Андрей");
         $("[data-test-id='phone'] .input__box .input__control[name='phone']").setValue("+12345678901");
         $("[data-test-id='agreement'] .checkbox__box").click();
         $(byText("Забронировать")).click();
         $("[data-test-id='notification']").shouldBe(Condition.visible, Duration.ofSeconds(15));
         $(byText("Успешно!")).shouldBe(Condition.visible);
-        $("[data-test-id='notification'] .notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + formatted));
+        $("[data-test-id='notification'] .notification__content").shouldHave
+                (Condition.text("Встреча успешно забронирована на " + meetDate));
+    }
+
+    @Test
+    @DisplayName("Meet reserved through 7 days")
+    void shouldReservedMeetThroughSevenDays() {
+
+//        Test config
+        String meetDate = date(7, "dd");
+        String fullMeetDate = date(7,"dd.MM.yyyy");
+
+//        Test data
+        $("[data-test-id='city'] .input__box .input__control[placeholder='Город']").setValue("Кр");
+        $(byText("Красноярск")).click();
+        $("[role ='button'] .icon-button__content .icon-button__text").click();
+//        SelenideElement dataStep = $("[data-step='1']");
+//        SelenideElement calendarDate = $(byText(meetDate)).shouldBe(Condition.disabled);
+        $(byText(meetDate)).click();
+        $("[data-test-id='name'] .input__box .input__control[name='name']").setValue("Семенов Андрей");
+        $("[data-test-id='phone'] .input__box .input__control[name='phone']").setValue("+12345678901");
+        $("[data-test-id='agreement'] .checkbox__box").click();
+        $(byText("Забронировать")).click();
+        $("[data-test-id='notification']").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(byText("Успешно!")).shouldBe(Condition.visible);
+        $("[data-test-id='notification'] .notification__content").shouldHave
+                                                        (Condition.text("Встреча успешно забронирована на " + fullMeetDate));
     }
 }
